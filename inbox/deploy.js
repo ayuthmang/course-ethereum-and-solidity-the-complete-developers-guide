893dfbb6ffd99a1b85797de3bf954f1760f7679e
Ayuth Mangmesap <ayuth.mang@gmail.com>
2021-06-27 10:55:16 +0700

    Section 2: Smart Contracts with Solidity

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,25 @@
 // deploy code will go here
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'prepare sample live bunker like usual celery era certain present page rate',
+  'https://rinkeby.infura.io/v3/7588d14cbe464392913f6ea460357dc8'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log(accounts);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!'],
+    })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
